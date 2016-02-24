c521c6660a02ca3d6df51997e46f9f972ea0a156
etrombly <etrombly@yahoo.com>
2016-02-24 16:22:32 +0100

    cleaned up some stuff in aur_repo

--- aur_repo.py
+++ aur_repo.py
@@ -7,11 +7,10 @@ buildPath = "/build"
 repoPath = "/repo"
 repoName = "aur-local"
 
-sudo.pacman("--noconfirm", "-Syyu")
+sudo.pacman("--noconfirm", "-Syu")
 
 pkglist = open("/etc/aur_repo/pkglist").read().split("\n")
 pkglist = filter(None, pkglist)
-pkgs = []
 for pkg in pkglist:
     tmp = Package.Package(pkg, buildPath, repoPath, repoName)
     tmp.build()
