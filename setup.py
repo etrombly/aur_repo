7b54081d91e8fb3160c2454188326230e9d53e4d
etrombly <etrombly@yahoo.com>
2016-02-25 10:55:07 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='aur_repo',
+    version='0.1.0',
+    author='etrombly',
+    author_email='etrombly@gmail.com',
+    packages=['Package'],
+    scripts=['bin/aur_repo'],
+    url='https://www.github.com/etrombly/aur_repo',
+    license='LICENSE',
+    description='Build local repo from aur packages',
+    long_description="",
+    install_requires=[
+        "sh",
+        "requests",
+    ],
+)
